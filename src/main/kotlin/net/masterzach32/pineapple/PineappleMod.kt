849011a3f9f3187ae59052683b9730dfaa1d33db
Zach <zachkozar@masterzach32.net>
2018-05-22 16:22:09 -0400

    Updated forge to 1.12.2

---
+++ src/main/kotlin/net/masterzach32/pineapple/PineappleMod.kt
@@ -0,0 +1,20 @@
+package net.masterzach32.pineapple
+
+import net.minecraftforge.fml.common.Mod
+
+/*
+ * PineappleMod - Created on 5/22/2018
+ * Author: Zach Kozar
+ * 
+ * This code is licensed under the GNU GPL v3
+ * You can find more info in the LICENSE file at the project root.
+ */
+
+/**
+ * @author Zach Kozar
+ * @version 5/22/2018
+ */
+@Mod(modid = "pineapple", name = "Pineapple Mod", version = "3.0.0", modLanguage = "kotlin")
+object PineappleMod {
+
+}
